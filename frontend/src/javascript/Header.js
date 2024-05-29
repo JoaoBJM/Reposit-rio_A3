8d146c9f4c6def09491586ca307049a86a268893
Matheus Araujo de Carvalho <matheusacgg@gmail.com>
2024-05-29 19:46:32 -0300

    Adicionado Backend + Frontend

--- frontend/src/javascript/Header.js
+++ frontend/src/javascript/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Logo from './componentes_landing/images/logo/reescreva-high-resolution-logo-transparent.png'
 
 const Header = (props) => {
     return (
         /* CABEÇALHO */
         <header className='main-header'>
-            <a className='main-header-title' href="/projetoa3_reescreva/src/html/index.js">Reescreva </a>
+            <a className='main-header-title' href="/projetoa3_reescreva/src/html/index.js"><img src={Logo}></img></a>
 
         </header>
 
